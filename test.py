5b4491f8a6f31451df8b60c68689db894f578d89
Jay Kukadiya <jkk9533@nyu.edu>
2023-12-03 20:19:18 -0500

    testing github co-pilot

---
+++ test.py
@@ -0,0 +1,20 @@
+#find a function to write fibonacci series
+#fibonacci series is a series of numbers where a number is the sum of the two numbers before it
+#the first two numbers are 0 and 1
+#the third number is 0+1=1
+#the fourth number is 1+1=2
+#the fifth number is 1+2=3
+#the sixth number is 2+3=5
+#the seventh number is 3+5=8
+#the eighth number is 5+8=13
+#and so on
+#the function should take a single parameter n
+#the function should return the nth value in the fibonacci series
+#the function should be called fib
+#the function should be defined in a separate file called fibonacci.py
+#the function should be imported into the current file using the import command
+#the function should be called using the dot notation
+#the function should be called using the dot notation
+
+import fibonacci
+fibonacci.fib(10)
