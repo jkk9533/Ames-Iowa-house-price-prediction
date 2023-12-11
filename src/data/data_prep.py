a356ccbe0f585a60386b54c84ec37f3ab5a33145
Jay Kukadiya <jkk9533@nyu.edu>
2023-12-10 23:03:44 -0500

    configured .env and added environment_variables.py

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -5,5 +5,4 @@ import matplotlib.pyplot as plt
 
 dataframe = pd.DataFrame(np.array([1,2,3,4,5,6,7,8,9,10]), columns=['x'])
 
-dataframe
 
---
+++ src/data/environment_variables.py
@@ -0,0 +1,22 @@
+
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+dotenv_path = find_dotenv()
+
+if dotenv_path:
+    load_dotenv(dotenv_path)
+else:
+    print("No .env file found. Using environment variables instead.")
+
+# Environment variables
+ENVIRONMENT = os.getenv("ENVIRONMENT")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+
+
+
+
+
